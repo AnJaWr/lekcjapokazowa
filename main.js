a2938ff6196fca538ef3d834d6d56c032667b096
anjawr <48285666+AnJaWr@users.noreply.github.com>
2023-08-25 16:27:38 +0200

    video

--- main.js
+++ main.js
@@ -4,9 +4,12 @@ var upper = document.getElementsByClassName("upper_wrapper");
 var bottom =document.getElementsByClassName("bottom_wrapper");
 
 if (userAgent.match(/iPad/i) || userAgent.match(/iPhone/i)) {
- paralax.classList.add("attached");
- upper.classList.add("attached");
- bottom.classList.add("attached");
+    paralax.classList.remove("paralax");
+ paralax.classList.add("paralax_bck");
+upper.classList.remove(".upper_wrapper");
+ upper.classList.add(".bottom_wrapper_bck");
+ bottom.classList.remove(".bottom_wrapper");
+bottom.classList.add(".bottom_wrapper_bck");
  console.log("safariiiiiiiiiiii");
 }
 else{
