eb938275cf382669cb71a947f07563a9dc725be9
anjawr <48285666+AnJaWr@users.noreply.github.com>
2023-08-25 11:40:17 +0200

    safari?!

--- main.js
+++ main.js
@@ -4,10 +4,10 @@ var upper = document.getElementsByClassName("upper_wrapper");
 var bottom =document.getElementsByClassName("bottom_wrapper");
 
 if (userAgent.match(/iPad/i) || userAgent.match(/iPhone/i)) {
- paralax.classList.add("attached")
- upper.classList.add("attached")
- bottom.classList.add("attached")
- console.log("safariiiiiiiiiiii")
+ paralax.classList.add("attached");
+ upper.classList.add("attached");
+ bottom.classList.add("attached");
+ console.log("safariiiiiiiiiiii");
 }
 else{
 console.log("not safariiiiiiiiii")
